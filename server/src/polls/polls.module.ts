6d3dbba73c54c3908a8fa3ada5c259f5ffa1d488
NinaBredova <innawonkaarm@gmail.com>
2022-12-12 18:12:02 +0400

    add edits

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { jwtModule, redisModule } from '../modules.config';
 import { PollsController } from './polls.controller';
+import { PollsRepository } from './polls.repository';
 import { PollsService } from './polls.service';
 
 @Module({
   imports: [ConfigModule, redisModule, jwtModule],
   controllers: [PollsController],
-  providers: [PollsService],
+  providers: [PollsService, PollsRepository],
 })
 export class PollsModule {}
